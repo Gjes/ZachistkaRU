050f48d9e59b02c21708813571cb5ea32f820644
Triada <tabenkov@yandex.ru>
2015-04-05 23:52:30 +0300

    Вызов поддержки и cargodrop
    Прилетает правильная техника после её вызова

--- zachistka_co36.Altis/comm_menu.h
+++ zachistka_co36.Altis/comm_menu.h
@@ -2,18 +2,18 @@ class CfgCommunicationMenu
 {
 	class AH99
 	{
-		text = "AH-99 Support"; // Text displayed in the menu and in a notification
+		text = "Mi-48 Support"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_helosupport = [(getmarkerpos ""ghst_player_support""),""B_Heli_Attack_01_F"",30,[400, 150],30,[false,""ColorRed""],""ghst_helosup""] spawn ghst_fnc_airsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_helosupport = [(getmarkerpos ""ghst_player_support""),""O_Heli_Attack_02_black_F"",30,[400, 150],30,[false,""ColorRed""],""ghst_helosup""] spawn ghst_fnc_airsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\attack_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
 	};
 	class A164
 	{
-		text = "A-164 Support"; // Text displayed in the menu and in a notification
+		text = "To-199 Support"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_cassupport = [(getmarkerpos ""ghst_player_support""),""B_Plane_CAS_01_F"",30,[600, 600],30,[false,""ColorRed""],""ghst_cassup""] spawn ghst_fnc_airsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_cassupport = [(getmarkerpos ""ghst_player_support""),""O_Plane_CAS_02_F"",30,[600, 600],30,[false,""ColorRed""],""ghst_cassup""] spawn ghst_fnc_airsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\cas_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
@@ -22,7 +22,7 @@ class CfgCommunicationMenu
 	{
 		text = "UAV Support"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_uavsupport = [(getmarkerpos ""ghst_player_support""),""B_UAV_02_F"",500,30] spawn ghst_fnc_uavsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_uavsupport = [(getmarkerpos ""ghst_player_support""),""O_UAV_02_F"",500,30] spawn ghst_fnc_uavsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\call_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
@@ -31,7 +31,7 @@ class CfgCommunicationMenu
 	{
 		text = "AR-2 Darter Support"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_puavsupport = [""B_UAV_01_F"",5] spawn ghst_fnc_puavsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_puavsupport = [""O_UAV_01_F"",5] spawn ghst_fnc_puavsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "";//"\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\call_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "";//"\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
@@ -40,7 +40,7 @@ class CfgCommunicationMenu
 	{
 		text = "UGV Support"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_ugvsupport = [(getmarkerpos ""ghst_player_support""),""B_UGV_01_rcws_F"",2,30] spawn ghst_fnc_ugvsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_ugvsupport = [(getmarkerpos ""ghst_player_support""),""O_UGV_01_rcws_F"",2,30] spawn ghst_fnc_ugvsupport;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\supplydrop_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
@@ -49,7 +49,7 @@ class CfgCommunicationMenu
 	{
 		text = "Ammo Drop"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_ammodrop = [player,(getmarkerpos ""ghst_player_support""),""B_Heli_Transport_03_F"",""B_supplyCrate_F"",150,30] spawn ghst_fnc_ammodrop;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_ammodrop = [player,(getmarkerpos ""ghst_player_support""),""O_Heli_Transport_04_F"",""O_supplyCrate_F"",150,30] spawn ghst_fnc_ammodrop;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\supplydrop_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
@@ -58,7 +58,7 @@ class CfgCommunicationMenu
 	{
 		text = "Cargo Drop"; // Text displayed in the menu and in a notification
 		submenu = ""; // Submenu opened upon activation
-		expression = "ghst_drop = [player,(getmarkerpos ""ghst_player_support""),""B_Heli_Transport_03_F"",200,30] spawn ghst_fnc_cargodrop;"; // Code executed upon activation (ignored when the submenu is not empty)
+		expression = "ghst_drop = [player,(getmarkerpos ""ghst_player_support""),""O_Heli_Transport_04_F"",200,30] spawn ghst_fnc_cargodrop;"; // Code executed upon activation (ignored when the submenu is not empty)
 		icon = "";//"\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\supplydrop_ca.paa"; // Icon displayed permanently next to the command menu
 		cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa"; // Custom cursor displayed when the item is selected
 		enable = "1"; // Simple expression condition for enabling the item
